426f4e0583c66dd7fa372b53ae24b04426153a51
Mindexperiment <andrewloyd@libero.it>
2019-05-01 19:36:18 +0200

    define advertisement test methods

--- tests/Feature/AdvertisementTest.php
+++ tests/Feature/AdvertisementTest.php
@@ -6,4 +6,48 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class AdvertisementTest extends TestCase {}
+use App\User;
+use App\Business;
+use App\Adv;
+
+class AdvertisementTest extends TestCase {
+
+ use RefreshDatabase;
+
+ public function test_User_EndpointClickUpdateAdvClicks_HasOneMoreClick() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->create( [ 'user_id'=>$user->id ] );
+  $adv = factory( Adv::class )->create( [ 'user_id'=>$user->id ] );
+
+  $response = $this->get( route( 'website.endpoint', $adv ) );
+
+  $this->assertDatabaseHas( 'advs', [ 'id'=>$adv->id, 'clicks'=>1 ] );
+ }
+
+ public function test_SubscribedUser_CanInsertAdv_HasAdv() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->create( [ 'user_id'=>$user->id ] );
+  $data = [ 'plan'=>'plan_EHouRyzHQYawXA', 'stripeToken'=>'tok_visa' ];
+  $this->post( route( 'plans.subscribe' ), $data );
+
+  $adv = factory( Adv::class )->make();
+  $response = $this->post( route( 'advs.store' ), $adv->toArray() );
+
+  $this->assertDatabaseHas( 'advs', [ 'title'=>$adv->title ] );
+ }
+
+ public function test_SubscribedUser_CanDeleteAdv_IsSoftDeleted() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->create( [ 'user_id'=>$user->id ] );
+  $data = [ 'plan'=>'plan_EHouRyzHQYawXA', 'stripeToken'=>'tok_visa' ];
+  $this->post( route( 'plans.subscribe' ), $data );
+  $adv = factory( Adv::class )->create();
+
+  $response = $this->delete( route( 'advs.destroy', $adv ) );
+
+  $this->assertSoftDeleted( 'advs', [ 'id'=>$adv->id ] );
+ }
+}
