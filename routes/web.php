31e6c1c19c20bde2b4bfdf8d2e407771bc75de04
Mindexperiment <andrewloyd@libero.it>
2019-05-01 16:40:25 +0200

    open plans routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,14 @@ Route::get( '/account/business', 'AccountController@business' )->name( 'account.
 // - business
 Route::post( '/account/business', 'BusinessController@store' )->name( 'business.store' );
 Route::get( '/business', 'BusinessController@index' )->name( 'business.index' );
+Route::get( '/business/subscriptions', 'BusinessController@subscriptions' )->name( 'business.subscriptions' );
+
+// - plans
+Route::post( '/plans', 'PlansController@store' )->name( 'plans.store' );
+Route::post( '/plans/subscribe', 'PlansController@subscribe' )->name( 'plans.subscribe' );
+Route::patch( '/plans/{plan}', 'PlansController@update' )->name( 'plans.update' );
+Route::patch( '/plans/{plan}/swap', 'PlansController@swap' )->name( 'plans.swap' );
+Route::delete( '/plans/{plan}', 'PlansController@destroy' )->name( 'plans.destroy' );
+Route::delete( '/plans/{plan}/cancel', 'PlansController@cancel' )->name( 'plans.cancel' );
+Route::get( '/plans', 'PlansController@index' )->name( 'plans.index' );
+Route::get( '/plans/create', 'PlansController@create' )->name( 'plans.create' );
