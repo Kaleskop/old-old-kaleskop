4d9f6cf8510054087988b5c3681f5b5ca43ceecc
Mindexperiment <andrewloyd@libero.it>
2019-05-01 20:17:10 +0200

    add advertisement model

---
+++ app/Adv.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Adv extends Model {
+
+ use SoftDeletes;
+
+ /**
+  * The table associated with the model.
+  *
+  * @var string
+  */
+ protected $table = 'advs';
+
+ /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+ protected $fillable = [ 'title', 'endpoint', 'clicks', 'published_at' ];
+
+ /**
+  * Register methods when booting model
+  */
+ public static function boot() {
+  parent::boot();
+
+  static::creating( function( $model ) {
+   $model->clicks = 0;
+  } );
+ }
+
+
+ // - relations
+
+ /**
+  * An adv belongs to a brand
+  *
+  * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+  */
+ public function owner() {
+  return $this->belongsTo( User::class, 'user_id' );
+ }
+}
