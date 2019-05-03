297304ca8a24d51228e8e40cb31ce807cd88ffee
Mindexperiment <andrewloyd@libero.it>
2019-05-03 13:39:42 +0200

    add website controller

---
+++ app/Http/Controllers/WebsiteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebsiteController extends Controller {
+
+ // - view actions
+
+ public function homepage() {
+  return '';
+ }
+}
