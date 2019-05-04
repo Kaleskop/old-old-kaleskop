bdab2f84fd9f5ea486126c63b216df5371b544e4
Mindexperiment <andrewloyd@libero.it>
2019-05-04 13:46:50 +0200

    define brands test methods

--- tests/Feature/BrandsTest.php
+++ tests/Feature/BrandsTest.php
@@ -6,4 +6,56 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class BrandsTest extends TestCase {}
+use App\Brand;
+use App\User;
+use App\Business;
+
+class BrandsTest extends TestCase {
+
+ use RefreshDatabase;
+
+ public function test_AuthUser_CannotRegisterBrands_AssertRedirect() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $brand = factory( Brand::class )->make();
+
+  $response = $this->post( route( 'brands.store' ), $brand->toArray() );
+
+  $response->assertRedirect( route( 'account.business' ) );
+ }
+
+ public function test_BusinessUser_CanInsertABrand_HasBrand() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->create( [ 'user_id'=>$user->id ] );
+
+  $brand = factory( Brand::class )->make( [ 'business_id'=>$business->id ] );
+  $response = $this->post( route( 'brands.store' ), $brand->toArray() );
+
+  $this->assertDatabaseHas( 'brands', [ 'name'=>$brand->name ] );
+ }
+
+ public function test_BusinessUser_CanEditBrandData_HasBrand() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->create( [ 'user_id'=>$user->id ] );
+  $brand = factory( Brand::class )->create( [ 'business_id'=>$business->id ] );
+
+  $data = $brand->toArray();
+  $data['name'] = 'Giuditta';
+  $response = $this->patch( route( 'brands.update', $brand ), $data );
+
+  $this->assertDatabaseHas( 'brands', [ 'name'=>$data['name'] ] );
+ }
+
+ public function test_BusinessUser_CanDeleteABrand_IsSoftDeleted() {
+  $user = factory( User::class )->create();
+  $this->actingAs( $user );
+  $business = factory( Business::class )->create( [ 'user_id'=>$user->id ] );
+  $brand = factory( Brand::class )->create( [ 'business_id'=>$business->id ]);
+
+  $response = $this->delete( route( 'brands.destroy', $brand ) );
+
+  $this->assertSoftDeleted( 'brands', [ 'id'=>$brand->id ] );
+ }
+}
