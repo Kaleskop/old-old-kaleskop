7ec4d37b3094ed1176f64a16b9c326ec56a9b43e
Mindexperiment <andrewloyd@libero.it>
2019-05-08 10:34:58 +0200

    open manifesto and sponsor section on advertisements

--- resources/views/advs/tile.blade.php
+++ resources/views/advs/tile.blade.php
@@ -2,4 +2,9 @@
  <h4>{{ $adv->title }}</h4>
 
  <p>{{ $adv->endpoint }}</p>
+
+ <footer>
+  <a href="{{ route( 'advs.manifesto', $adv ) }}">{{ __( 'Manifesto' ) }}</a>
+  <a href="{{ route( 'advs.sponsor', $adv ) }}">{{ __( 'Sponsor' ) }}</a>
+ </footer>
 </article>
