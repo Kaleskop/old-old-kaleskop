f1a82efa88bad02fafe78d554a0bd7af1b91afe5
Mindexperiment <andrewloyd@libero.it>
2019-05-09 12:26:03 +0200

    close #30 - add views for channels

---
+++ resources/views/website/advs-page.blade.php
@@ -0,0 +1,25 @@
+@extends( 'layouts.page' )
+
+@section( 'page' )
+<section>
+ <div>
+
+  <header>
+   <h2>{{ $adv->title }}</h2>
+
+   <a href="{{ route( 'website.endpoint', $adv ) }}"><span class="fas fa-external-link-square-alt"></span></a>
+  </header>
+ </div>
+
+ <div>
+  @foreach( $adv->brands as $brand )
+   @include( 'brands.tile-on-channels' )
+
+  @endforeach
+ </div>
+
+ @include( 'opineons.section' )
+
+</section>
+
+@endsection
