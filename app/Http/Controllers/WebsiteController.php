bf2d2cd25f01cec857479df3a97adce29464ffd6
Mindexperiment <andrewloyd@libero.it>
2019-05-09 12:16:45 +0200

    update website controller with channels methods

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Adv;
+use App\Brand;
 
 class WebsiteController extends Controller {
 
@@ -19,5 +20,18 @@ class WebsiteController extends Controller {
 
  public function homepage() {
   return '';
+ }
+
+ public function channels() {
+  $advs = Adv::latest()->get();
+
+  return view( 'layouts.wrapper', [ 'page'=>'website.channels-page' ] )
+   ->with( 'advs', $advs );
+ }
+
+ public function advs( Brand $brand, Adv $adv = null ) {
+  return view( 'layouts.wrapper', [ 'page'=>'website.advs-page' ] )
+   ->with( 'brand', $brand )
+   ->with( 'adv', $adv );
  }
 }
