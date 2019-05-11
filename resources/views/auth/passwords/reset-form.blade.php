27cc0049afb9c26f54f2f5089d1b5fc134b1825a
Mindexperiment <andrewloyd@libero.it>
2019-05-11 16:46:33 +0200

    add input style on passwords reset-form

--- resources/views/auth/passwords/reset-form.blade.php
+++ resources/views/auth/passwords/reset-form.blade.php
@@ -6,7 +6,7 @@
 
   <div>
    <label for="email" class="block p-2 font-semibold">{{ __( 'Email address' ) }}</label>
-   <input type="email" name="email" id="email" value="{{ $email ?? old('email') }}" required autofocus />
+   <input type="email" name="email" id="email" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white shadow focus:shadow-md" value="{{ $email ?? old('email') }}" required autofocus />
 
    @if ( $errors->has( 'email' ) )
     {{ $errors->first( 'email' ) }}
@@ -15,7 +15,7 @@
 
   <div class="mt-4">
    <label for="password" class="block p-2 font-semibold">{{ __( 'Password' ) }}</label>
-   <input type="password" name="password" id="password" required />
+   <input type="password" name="password" id="password" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white shadow focus:shadow-md" required />
 
    @if ( $errors->has( 'password' ) )
     {{ $errors->first( 'password' ) }}
@@ -24,7 +24,7 @@
 
   <div class="mt-4">
    <label for="password-confirm" class="block p-2 font-semibold">{{ __( 'Confirm password' ) }}</label>
-   <input type="password" name="password_confirmation" id="password-confirm" required />
+   <input type="password" name="password_confirmation" id="password-confirm" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white shadow focus:shadow-md" required />
   </div>
  </fieldset>
 
