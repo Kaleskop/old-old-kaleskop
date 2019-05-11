17bbf6a8d567a2668e6c51ef84a4fad00b1e6686
Mindexperiment <andrewloyd@libero.it>
2019-05-11 16:46:54 +0200

    add fieldset style on passwords reset-form

--- resources/views/auth/passwords/reset-form.blade.php
+++ resources/views/auth/passwords/reset-form.blade.php
@@ -1,7 +1,7 @@
 <form method="POST" action="{{ route('password.update') }}">
  @csrf
 
- <fieldset>
+ <fieldset class="mb-8">
   <legend class="p-2 font-light italic">{{ __( 'Account details' ) }}</legend>
 
   <div>
