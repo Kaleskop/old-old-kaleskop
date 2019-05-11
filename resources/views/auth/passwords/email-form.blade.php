ff45a5a8c5303bcd35c70be29da751b20cf7eae3
Mindexperiment <andrewloyd@libero.it>
2019-05-11 16:37:07 +0200

    add fieldset style on passwords email-form

--- resources/views/auth/passwords/email-form.blade.php
+++ resources/views/auth/passwords/email-form.blade.php
@@ -1,7 +1,7 @@
 <form method="POST" action="{{ route( 'password.email' ) }}">
  @csrf
 
- <fieldset>
+ <fieldset class="mb-8">
   <legend class="p-2 font-light italic">{{ __( 'Account details' ) }}</legend>
 
   <div>
