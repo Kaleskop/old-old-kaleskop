5d3d46a8183045568b41730eb552dca51de47f10
Mindexperiment <andrewloyd@libero.it>
2019-05-12 00:58:12 +0200

    add label style on brands update-form

--- resources/views/brands/update-form.blade.php
+++ resources/views/brands/update-form.blade.php
@@ -6,7 +6,7 @@
   <legend>{{ __( 'Brand details' ) }}</legend>
 
   <div>
-   <label for="name">{{ __( 'Name' ) }}</label>
+   <label for="name" class="block p-2 font-semibold">{{ __( 'Name' ) }}</label>
    <input type="text" name="name" id="name" value="{{ $brand->name }}" required autofocus />
 
    @if ( $errors->has( 'name' ) )
@@ -15,7 +15,7 @@
   </div>
 
   <div>
-   <label for="description">{{ __( 'Description' ) }}</label>
+   <label for="description" class="block p-2 font-semibold">{{ __( 'Description' ) }}</label>
    <textarea name="description" id="description">{{ $brand->description }}</textarea>
 
    @if ( $errors->has( 'description' ) )
@@ -24,7 +24,7 @@
   </div>
 
   <div>
-   <label for="userfile">{{ __( 'Choose Logo' ) }}</label>
+   <label for="userfile" class="block p-2 font-semibold">{{ __( 'Choose Logo' ) }}</label>
    <input type="file" name="userfile" accept="image/*" id="userfile" />
 
    @if ( $errors->has( 'userfile' ) )
