2a170549d573c54d511b042fbb2bffbe064a1642
Mindexperiment <andrewloyd@libero.it>
2019-05-11 16:25:39 +0200

    add button style on auth logout-form

--- resources/views/auth/logout-form.blade.php
+++ resources/views/auth/logout-form.blade.php
@@ -1,5 +1,5 @@
 <form method="POST" action="{{ route( 'logout' ) }}">
  @csrf
 
- <button type="submit">{{ __( 'Logout' ) }}</button>
+ <button type="submit" class="appearance-none outline-none cursor-pointer select-none block leading-snug py-1 px-2 border border-transparent text-lg font-medium focus:shadow-md">{{ __( 'Logout' ) }}</button>
 </form>
