0502a73beca407ade4dd1035cc59e11f22fa4398
Mindexperiment <andrewloyd@libero.it>
2019-05-12 14:26:00 +0200

    add input style on advs create-form

--- resources/views/advs/create-form.blade.php
+++ resources/views/advs/create-form.blade.php
@@ -6,7 +6,7 @@
 
   <div>
    <label for="title" class="block p-2 font-semibold">{{ __( 'Title' ) }}</label>
-   <input type="text" name="title" id="title" value="{{ old( 'title' ) }}" required autofocus />
+   <input type="text" name="title" id="title" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white shadow focus:shadow-md" value="{{ old( 'title' ) }}" required autofocus />
 
    @if ( $errors->has( 'title' ) )
     {{ $errors->first( 'title' ) }}
@@ -15,7 +15,7 @@
 
   <div class="mt-4">
    <label for="endpoint" class="block p-2 font-semibold">{{ __( 'Endpoint' ) }}</label>
-   <input type="text" name="endpoint" id="endpoint" value="{{ old( 'endpoint' ) }}" required />
+   <input type="text" name="endpoint" id="endpoint" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white shadow focus:shadow-md" value="{{ old( 'endpoint' ) }}" required />
 
    @if ( $errors->has( 'endpoint' ) )
     {{ $errors->first( 'endpoint' ) }}
