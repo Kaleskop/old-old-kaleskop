b623d0bb82226e2da816471886ad6014ff5fd1b1
Mindexperiment <andrewloyd@libero.it>
2019-05-12 14:01:46 +0200

    add input style on plans create-form

--- resources/views/plans/create-form.blade.php
+++ resources/views/plans/create-form.blade.php
@@ -6,7 +6,7 @@
 
   <div>
    <label for="name" class="block p-2 font-semibold">{{ __( 'Name' ) }}</label>
-   <input type="text" name="name" id="name" value="{{ old( 'name' ) }}" required autofocus />
+   <input type="text" name="name" id="name" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white text-base font-normal shadow focus:shadow-md" value="{{ old( 'name' ) }}" required autofocus />
 
    @if ( $errors->has( 'name' ) )
     {{ $errors->first( 'name' ) }}
@@ -37,7 +37,7 @@
 
   <div>
    <label for="product_id" class="block p-2 font-semibold">{{ __( 'Product id' ) }}</label>
-   <input type="text" name="product_id" id="product_id" value="{{ old( 'product_id' ) }}" required />
+   <input type="text" name="product_id" id="product_id" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white text-base font-normal shadow focus:shadow-md" value="{{ old( 'product_id' ) }}" required />
 
    @if ( $errors->has( 'product_id' ) )
     {{ $errors->first( 'product_id' ) }}
@@ -46,7 +46,7 @@
 
   <div class="mt-4">
    <label for="product_name" class="block p-2 font-semibold">{{ __( 'Product name' ) }}</label>
-   <input type="text" name="product_name" id="product_name" value="{{ old( 'product_name' ) }}" required />
+   <input type="text" name="product_name" id="product_name" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white text-base font-normal shadow focus:shadow-md" value="{{ old( 'product_name' ) }}" required />
 
    @if ( $errors->has( 'product_name' ) )
     {{ $errors->first( 'product_name' ) }}
@@ -55,7 +55,7 @@
 
   <div class="mt-4">
    <label for="plan_id" class="block p-2 font-semibold">{{ __( 'Plan id' ) }}</label>
-   <input type="text" name="plan_id" id="plan_id" value="{{ old( 'plan_id' ) }}" required />
+   <input type="text" name="plan_id" id="plan_id" class="appearance-none outline-none cursor-text block w-full leading-snug py-1 px-2 rounded border border-transparent bg-white text-base font-normal shadow focus:shadow-md" value="{{ old( 'plan_id' ) }}" required />
 
    @if ( $errors->has( 'plan_id' ) )
     {{ $errors->first( 'plan_id' ) }}
