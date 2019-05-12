d573c34119d7da72af78093bc9f0781db0e5ab78
Mindexperiment <andrewloyd@libero.it>
2019-05-12 13:59:22 +0200

    add label style on plans create-form

--- resources/views/plans/create-form.blade.php
+++ resources/views/plans/create-form.blade.php
@@ -5,7 +5,7 @@
   <legend>{{ __( 'Plan details' ) }}</legend>
 
   <div>
-   <label for="name">{{ __( 'Name' ) }}</label>
+   <label for="name" class="block p-2 font-semibold">{{ __( 'Name' ) }}</label>
    <input type="text" name="name" id="name" value="{{ old( 'name' ) }}" required autofocus />
 
    @if ( $errors->has( 'name' ) )
@@ -14,7 +14,7 @@
   </div>
 
   <div>
-   <label for="description">{{ __( 'Description' ) }}</label>
+   <label for="description" class="block p-2 font-semibold">{{ __( 'Description' ) }}</label>
    <textarea name="description" id="description">{{ old( 'description' ) }}</textarea>
 
    @if ( $errors->has( 'description' ) )
@@ -23,7 +23,7 @@
   </div>
 
   <div>
-   <label for="price">{{ __( 'Price' ) }}</label>
+   <label for="price" class="block p-2 font-semibold">{{ __( 'Price' ) }}</label>
    <input type="number" min="0" step="0.01" name="price" id="price" value="{{ old( 'price' ) }}" required />
 
    @if ( $errors->has( 'price' ) )
@@ -36,7 +36,7 @@
   <legend>{{ __( 'Plan details' ) }}</legend>
 
   <div>
-   <label for="product_id">{{ __( 'Product id' ) }}</label>
+   <label for="product_id" class="block p-2 font-semibold">{{ __( 'Product id' ) }}</label>
    <input type="text" name="product_id" id="product_id" value="{{ old( 'product_id' ) }}" required />
 
    @if ( $errors->has( 'product_id' ) )
@@ -45,7 +45,7 @@
   </div>
 
   <div>
-   <label for="product_name">{{ __( 'Product name' ) }}</label>
+   <label for="product_name" class="block p-2 font-semibold">{{ __( 'Product name' ) }}</label>
    <input type="text" name="product_name" id="product_name" value="{{ old( 'product_name' ) }}" required />
 
    @if ( $errors->has( 'product_name' ) )
@@ -54,7 +54,7 @@
   </div>
 
   <div>
-   <label for="plan_id">{{ __( 'Plan id' ) }}</label>
+   <label for="plan_id" class="block p-2 font-semibold">{{ __( 'Plan id' ) }}</label>
    <input type="text" name="plan_id" id="plan_id" value="{{ old( 'plan_id' ) }}" required />
 
    @if ( $errors->has( 'plan_id' ) )
