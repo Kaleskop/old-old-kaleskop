7f5822ab97a4eb686420e1e8d78776b31f1d6131
Mindexperiment <andrewloyd@libero.it>
2019-05-15 17:53:33 +0200

    add h4 style on comments section

--- resources/views/comments/section.blade.php
+++ resources/views/comments/section.blade.php
@@ -1,5 +1,5 @@
 <section class="p-2">
- <h4>{{ __( 'Comments' ) }}</h4>
+ <h4 class="mb-2 font-medium text-sm text-gray-800 uppercase">{{ __( 'Comments' ) }}</h4>
 
  @auth
   @include( 'comments.create-form' )
