b0f1dd99a25a5488f9c382a2204bf612968a3296
Mindexperiment <andrewloyd@libero.it>
2019-05-15 11:21:34 +0200

    add select style on advs sponsor-form

--- resources/views/advs/sponsor-form.blade.php
+++ resources/views/advs/sponsor-form.blade.php
@@ -8,7 +8,7 @@
 
   <div>
    <label for="brands" class="block p-2 font-semibold">{{ __( 'Brands' ) }}</label>
-   <select name="brands[]" id="brands" multiple>
+   <select name="brands[]" id="brands" class="appearance-none outline-none cursor-pointer block w-full leading-snug py-1 px-2 border border-transparent bg-white shadow focus:shadow-md" multiple>
     @foreach( $brands as $brand )
      <option value="{{ $brand->id }}"{{ in_array( $brand->id, $adv->brands()->pluck( 'id' )->toArray()) ? ' selected' : '' }}>{{ $brand->name }}</option>
 
