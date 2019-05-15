e9da2deeb86c91c3f179f45b1aecf3e1c3b17b72
Mindexperiment <andrewloyd@libero.it>
2019-05-16 00:23:21 +0200

    add span style on layouts menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,29 +3,29 @@
  @auth
  <a href="{{ route( 'account.index' ) }}">
   <span class="far fa-user"></span>
-  <span>{{ __( 'Account' ) }}</span>
+  <span class="hidden">{{ __( 'Account' ) }}</span>
  </a>
  <a href="{{ route( 'business.index' ) }}">
   <span class="far fa-building"></span>
-  <span>{{ __( 'Business' ) }}</span>
+  <span class="hidden">{{ __( 'Business' ) }}</span>
  </a>
  @endauth
 
  @business
  <a href="{{ route( 'brands.index' ) }}">
   <span class="fas fa-tags"></span>
-  <span>{{ __( 'Brands' ) }}</span>
+  <span class="hidden">{{ __( 'Brands' ) }}</span>
  </a>
  <a href="{{ route( 'advs.index' ) }}">
   <span class="fas fa-play"></span>
-  <span>{{ __( 'Advertisements' ) }}</span>
+  <span class="hidden">{{ __( 'Advertisements' ) }}</span>
  </a>
  @endbusiness
 
  @subscribed
  <a href="{{ route( 'videos.index' ) }}">
   <span class="fas fa-film"></span>
-  <span>{{ __( 'Videos' ) }}</span>
+  <span class="hidden">{{ __( 'Videos' ) }}</span>
  </a>
  @endsubscribed
 
