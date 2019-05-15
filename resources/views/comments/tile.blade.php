4ac0d0845286f5726e43a8e7fe40685c9b59ca0a
Mindexperiment <andrewloyd@libero.it>
2019-05-15 17:56:04 +0200

    add content style on comments section

--- resources/views/comments/tile.blade.php
+++ resources/views/comments/tile.blade.php
@@ -1,7 +1,7 @@
 <div class="relative py-4 flex">
  <figure class="w-8 h-8 mr-2 rounded-full"><!-- user picture here --></figure>
 
- <div>
+ <div class="flex-1">
   <p>{{ $comment->body }}</p>
  </div>
 </div>
