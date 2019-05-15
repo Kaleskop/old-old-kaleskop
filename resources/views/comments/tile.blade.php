0c065208db3bc3072c4e6ce48434ae083fc99a21
Mindexperiment <andrewloyd@libero.it>
2019-05-15 17:55:31 +0200

    add figure style on comments section

--- resources/views/comments/tile.blade.php
+++ resources/views/comments/tile.blade.php
@@ -1,5 +1,5 @@
 <div class="relative py-4 flex">
- <figure><!-- user picture here --></figure>
+ <figure class="w-8 h-8 mr-2 rounded-full"><!-- user picture here --></figure>
 
  <div>
   <p>{{ $comment->body }}</p>
