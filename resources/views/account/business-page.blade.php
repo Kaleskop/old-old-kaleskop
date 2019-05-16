899d61ad9f8a271a635cc83f3406b9b2333fe291
Mindexperiment <andrewloyd@libero.it>
2019-05-16 16:46:14 +0200

    add h2 style on account sections

--- resources/views/account/business-page.blade.php
+++ resources/views/account/business-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'Business' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Business' ) }}</h2>
  </header>
 
  @include( 'account.business-form' )
--- resources/views/account/index-page.blade.php
+++ resources/views/account/index-page.blade.php
@@ -2,7 +2,7 @@
 
 @section( 'page' )
 <section class="px-4 py-8">
- <h2>{{ __( 'Hi, :username', [ 'username'=>$user->name ] ) }}</h2>
+ <h2 class="mb-2 font-medium text-3xl">{{ __( 'Hi, :username', [ 'username'=>$user->name ] ) }}</h2>
 
  @include( 'account.verified-email-message' )
 
