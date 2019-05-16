aa02c230def9788f41afe0470f6eca8638a1e4aa
Mindexperiment <andrewloyd@libero.it>
2019-05-16 20:58:45 +0200

    add h2 style on advs sections

--- resources/views/advs/index-page.blade.php
+++ resources/views/advs/index-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'Advertisements' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Advertisements' ) }}</h2>
 
   <a href="{{ route( 'advs.create' ) }}" class="underline">{{ __( 'New advertisement' ) }}</a>
  </header>
--- resources/views/advs/manifesto-page.blade.php
+++ resources/views/advs/manifesto-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'New manifesto' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'New manifesto' ) }}</h2>
  </header>
 
  @if ( count( $videos ) > 0 )
