71b5a81b05ffc69446aba40fd5cf7dbd2289dcca
Mindexperiment <andrewloyd@libero.it>
2019-05-16 17:18:37 +0200

    add h2 style on business sections

--- resources/views/business/index-page.blade.php
+++ resources/views/business/index-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ $business->name }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ $business->name }}</h2>
  </header>
 </section>
 
--- resources/views/business/subscriptions-page.blade.php
+++ resources/views/business/subscriptions-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'Subscriptions' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Subscriptions' ) }}</h2>
  </header>
 
  @include( 'business.subscription-form' )
