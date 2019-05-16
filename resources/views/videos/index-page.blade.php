316757bc584f98c74b3965223a3e7f2d6cec6758
Mindexperiment <andrewloyd@libero.it>
2019-05-16 17:45:01 +0200

    add header style on videos sections

--- resources/views/videos/index-page.blade.php
+++ resources/views/videos/index-page.blade.php
@@ -2,7 +2,7 @@
 
 @section( 'page' )
 <section class="px-4 py-8">
- <header>
+ <header class="mb-4">
   <h2>{{ __( 'Videos' ) }}</h2>
 
   <a href="{{ route( 'videos.create' ) }}">{{ __( 'New Video' ) }}</a>
