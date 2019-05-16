2dbadfb055d6cdd7671870c35b48a829cc520d3a
Mindexperiment <andrewloyd@libero.it>
2019-05-16 17:46:36 +0200

    add h2 style on videos sections

--- resources/views/videos/index-page.blade.php
+++ resources/views/videos/index-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'Videos' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Videos' ) }}</h2>
 
   <a href="{{ route( 'videos.create' ) }}" class="underline">{{ __( 'New Video' ) }}</a>
  </header>
