35be4250b3c74cb51a1e33d540e63973eed810c2
Mindexperiment <andrewloyd@libero.it>
2019-05-16 16:45:00 +0200

    add section style on account sections

--- resources/views/account/index-page.blade.php
+++ resources/views/account/index-page.blade.php
@@ -1,7 +1,7 @@
 @extends( 'layouts.page' )
 
 @section( 'page' )
-<section>
+<section class="px-4 py-8">
  <h2>{{ __( 'Hi, :username', [ 'username'=>$user->name ] ) }}</h2>
 
  @include( 'account.verified-email-message' )
