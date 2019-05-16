ee10f908de34da263cca299e9c8024c7e2b7aea5
Mindexperiment <andrewloyd@libero.it>
2019-05-16 19:47:06 +0200

    add h2 style on brands sections

--- resources/views/brands/index-page.blade.php
+++ resources/views/brands/index-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'Brands' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Brands' ) }}</h2>
 
   <a href="{{ route( 'brands.create' ) }}" class="underline">{{ __( 'New brand' ) }}</a>
  </header>
--- resources/views/brands/sponsor-page.blade.php
+++ resources/views/brands/sponsor-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'New sponsor' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'New sponsor' ) }}</h2>
  </header>
 
  @if ( count( $advs ) > 0 )
