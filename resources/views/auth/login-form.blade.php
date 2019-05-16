f02d5cd693a4dcd0e3e17a2a7f89270df955b51f
Mindexperiment <andrewloyd@libero.it>
2019-05-16 15:47:21 +0200

    add button style on auth login-form

--- resources/views/auth/login-form.blade.php
+++ resources/views/auth/login-form.blade.php
@@ -36,5 +36,5 @@
   </div>
  </fieldset>
 
- <button type="submit">{{ __( 'Login' ) }}</button>
+ <button type="submit" class="appearance-none outline-none cursor-pointer select-none block w-full leading-snug py-1 px-2 border border-transparent bg-gray-400 text-lg font-semibold text-center focus:shadow-md">{{ __( 'Login' ) }}</button>
 </form>
