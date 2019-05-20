27d5847d985fe93760bf3148e58c7e7f23373ee1
Mindexperiment <andrewloyd@libero.it>
2019-05-20 17:12:01 +0200

    fix #55 - correct advs tile on channels page

--- resources/views/advs/tile-on-channels.blade.php
+++ resources/views/advs/tile-on-channels.blade.php
@@ -2,7 +2,7 @@
  <h4>{{ $adv->title }}</h4>
 
  @forelse( $adv->brands as $brand )
-  <a href="{{ route( 'website.advs', [ $brand, $adv ] ) }}">{{ $adv->title }}</a>
+  @include( 'brands.tile-on-adv' )
 
  @empty
   <p>{{ __( 'No brands' ) }}</p>
---
+++ resources/views/brands/tile-on-adv.blade.php
@@ -0,0 +1,7 @@
+<div>
+ <figure>
+  <img src="{{ asset( 'storage/'.$brand->logo_path ) }}" />
+
+  <caption>{{ $brand->name }}</caption>
+ </figure>
+</div>
