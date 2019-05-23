dc79b9ccf3301c0c9362e94641028b7ff5fa86ae
Mindexperiment <andrewloyd@libero.it>
2019-05-23 14:10:40 +0200

    fix #73 - manage empty items on advs sponsor page

--- resources/views/advs/sponsor-page.blade.php
+++ resources/views/advs/sponsor-page.blade.php
@@ -6,7 +6,13 @@
   <h2>{{ __( 'New sponsor' ) }}</h2>
  </header>
 
- @include( 'advs.sponsor-form' )
+ @if ( count( $advs ) > 0 )
+  @include( 'advs.sponsor-form' )
+
+ @else
+  <p>{{ __( 'You need to' ) }} <a href="{{ route( 'brands.create' ) }}">{{ __( 'insert a new brand' ) }}</a></p>
+
+ @endif
 
 </section>
 
