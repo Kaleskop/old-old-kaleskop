6ac9a6ee38abf257973c662ac23a99c1d630f944
Mindexperiment <andrewloyd@libero.it>
2019-05-23 14:10:13 +0200

    fix #74 - manage empty items on advs manifesto page

--- resources/views/advs/manifesto-page.blade.php
+++ resources/views/advs/manifesto-page.blade.php
@@ -6,7 +6,13 @@
   <h2>{{ __( 'New manifesto' ) }}</h2>
  </header>
 
- @include( 'advs.manifesto-form' )
+ @if ( count( $videos ) > 0 )
+  @include( 'advs.manifesto-form' )
+
+ @else
+  <p>{{ __( 'You need to' ) }} <a href="{{ route( 'videos.create' ) }}">{{ __( 'insert a new video' ) }}</a></p>
+
+ @endif
 
 </section>
 
