01b4fc32566f17c66d6b0b46a005c4eb56192ff2
Mindexperiment <andrewloyd@libero.it>
2019-05-23 17:21:42 +0200

    add header style on plans sections

--- resources/views/plans/index-page.blade.php
+++ resources/views/plans/index-page.blade.php
@@ -2,7 +2,7 @@
 
 @section( 'page' )
 <section class="px-4 py-8">
- <header>
+ <header class="mb-4">
   <h2>{{ __( 'Plans' ) }}</h2>
 
   <a href="{{ route( 'plans.create' ) }}">{{ __( 'New plan' ) }}</a>
