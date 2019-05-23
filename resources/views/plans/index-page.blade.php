dc5951ceec5c683fce0de0f49ed508c2c3d9d060
Mindexperiment <andrewloyd@libero.it>
2019-05-23 17:22:04 +0200

    add link style on plans sections

--- resources/views/plans/index-page.blade.php
+++ resources/views/plans/index-page.blade.php
@@ -5,7 +5,7 @@
  <header class="mb-4">
   <h2>{{ __( 'Plans' ) }}</h2>
 
-  <a href="{{ route( 'plans.create' ) }}">{{ __( 'New plan' ) }}</a>
+  <a href="{{ route( 'plans.create' ) }}" class="underline">{{ __( 'New plan' ) }}</a>
  </header>
 
  @include( 'plans.table' )
