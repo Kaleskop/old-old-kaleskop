52b40d45cd7f6ee83475e088ab967c50c9280b3e
Mindexperiment <andrewloyd@libero.it>
2019-05-23 17:22:39 +0200

    add h2 style on plans sections

--- resources/views/plans/index-page.blade.php
+++ resources/views/plans/index-page.blade.php
@@ -3,7 +3,7 @@
 @section( 'page' )
 <section class="px-4 py-8">
  <header class="mb-4">
-  <h2>{{ __( 'Plans' ) }}</h2>
+  <h2 class="mb-2 font-medium text-3xl">{{ __( 'Plans' ) }}</h2>
 
   <a href="{{ route( 'plans.create' ) }}" class="underline">{{ __( 'New plan' ) }}</a>
  </header>
