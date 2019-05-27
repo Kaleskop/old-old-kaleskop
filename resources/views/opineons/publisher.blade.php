7d2db9525852489a6b5f83eadb4bb169e6fb3009
Mindexperiment <andrewloyd@libero.it>
2019-05-27 11:10:49 +0200

    add figure style on opineons publisher

--- resources/views/opineons/publisher.blade.php
+++ resources/views/opineons/publisher.blade.php
@@ -1,6 +1,6 @@
 <div>
  <div>
-  <figure><!-- user picture here --></figure>
+  <figure class="w-8 h-8 mr-2 rounded-full"><!-- user picture here --></figure>
 
   @include( 'opineons.create-form' )
 
