851186b3232cd8c9b31f7148dc0c76b4c075ca0d
Mindexperiment <andrewloyd@libero.it>
2019-05-27 11:05:59 +0200

    add figure style on opineons feeds

--- resources/views/opineons/feed.blade.php
+++ resources/views/opineons/feed.blade.php
@@ -1,6 +1,6 @@
 <article class="pl-2 pr-4">
  <div class="relative py-4 flex">
-  <figure><!-- user picture here --></figure>
+  <figure class="w-8 h-8 mr-2 rounded-full"><!-- user picture here --></figure>
   
   <div class="p-2 flex-1">
    <p>{{ Illuminate\Mail\Markdown::parse( $opinion->body ) }}</p>
