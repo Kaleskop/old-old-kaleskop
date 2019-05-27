a17972ee6be3b458082cdf746c949cf841b19c6a
Mindexperiment <andrewloyd@libero.it>
2019-05-27 15:16:35 +0200

    add paragraph style on advs tile

--- resources/views/advs/tile.blade.php
+++ resources/views/advs/tile.blade.php
@@ -1,7 +1,7 @@
 <article>
  <h4 class="mb-2 font-medium text-2xl">{{ $adv->title }}</h4>
 
- <p>{{ $adv->endpoint }}</p>
+ <p class="mb-4 font-light text-sm">{{ $adv->endpoint }}</p>
 
  <footer class="flex justify-end">
   <a href="{{ route( 'advs.sponsor', $adv ) }}" class="underline mr-4">{{ __( 'Sponsor' ) }}</a>
