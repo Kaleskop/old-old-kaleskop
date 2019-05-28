259bcbaff35352371a8b90d6cafe39c23102617d
Mindexperiment <andrewloyd@libero.it>
2019-05-28 11:07:02 +0200

    add app.name style on passwords sections

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,7 +4,7 @@
 <section class="px-8 py-16">
  <header class="mb-4 text-center">
   <h2 class="mb-2 font-medium text-3xl">{{ __( 'Reset password' ) }}</h2>
-  <p>{{ config( 'app.name' ) }}</p>
+  <p class="font-semibold text-2xl uppercase">{{ config( 'app.name' ) }}</p>
  </header>
 
  @if ( session( 'status' ) )
