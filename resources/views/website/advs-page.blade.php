cc0427cc580a0429cbb5c08bc2c4bca71c7728ed
Mindexperiment <andrewloyd@libero.it>
2019-05-30 20:18:00 +0200

    update h2 style on website advs

--- resources/views/website/advs-page.blade.php
+++ resources/views/website/advs-page.blade.php
@@ -6,7 +6,7 @@
   <video-player v-bind:adv="{{ $adv }}"></video-player>
 
   <header class="p-2 flex justify-between items-center bg-black text-white">
-   <h2 class="mb-2 font-semibold text-2xl">{{ $adv->title }}</h2>
+   <h2 class="font-medium text-2xl">{{ $adv->title }}</h2>
 
    <a href="{{ route( 'website.endpoint', $adv ) }}"><span class="fas fa-external-link-square-alt"></span></a>
   </header>
