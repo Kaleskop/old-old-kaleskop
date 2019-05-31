0e7de98da0926c974dab54ab2546ec9ba535a4bc
Mindexperiment <andrewloyd@libero.it>
2019-05-31 16:56:34 +0200

    add link style on layouts header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header class="p-4">
  <h1 class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></h1>
 
- <a href="{{ route( 'website.channels' ) }}">
+ <a href="{{ route( 'website.channels' ) }}" class="sm:hidden">
   <span class="fas fa-project-diagram"></span>
   <span>{{ __( 'Channels' ) }}</span>
  </a>
