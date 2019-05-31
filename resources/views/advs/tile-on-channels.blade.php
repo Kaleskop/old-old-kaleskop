3e7fed52f19f3688af4dced274bae8b3c7eb4f98
Mindexperiment <andrewloyd@libero.it>
2019-05-31 14:30:46 +0200

    update structure on advs tile-on-channels

--- resources/views/advs/tile-on-channels.blade.php
+++ resources/views/advs/tile-on-channels.blade.php
@@ -1,11 +1,13 @@
 <article>
+ <div>
+  @forelse( $adv->brands as $brand )
+   @include( 'brands.tile-on-adv' )
+
+  @empty
+   <p>{{ __( 'No brands' ) }}</p>
+
+  @endforelse
+ </div>
+
  <h4>{{ $adv->title }}</h4>
-
- @forelse( $adv->brands as $brand )
-  @include( 'brands.tile-on-adv' )
-
- @empty
-  <p>{{ __( 'No brands' ) }}</p>
-
- @endforelse
 </article>
