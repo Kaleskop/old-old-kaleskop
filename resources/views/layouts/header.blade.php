40aa1e6abf75e22efe395bebc007b985039069be
Mindexperiment <andrewloyd@libero.it>
2019-05-31 16:54:21 +0200

    add channels link on layouts header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,8 @@
 <header class="p-4">
  <h1 class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></h1>
+
+ <a href="{{ route( 'website.channels' ) }}">
+  <span class="fas fa-project-diagram"></span>
+  <span>{{ __( 'Channels' ) }}</span>
+ </a>
 </header>
