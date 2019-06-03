df7b9ea74f8505744766ff6cff11b9f30682837c
Mindexperiment <andrewloyd@libero.it>
2019-06-03 17:26:12 +0200

    clear opineon controller of express method

--- app/Http/Controllers/OpineonController.php
+++ app/Http/Controllers/OpineonController.php
@@ -5,23 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Agpretto\Opineon\Opinion;
-use App\Adv;
 
 class OpineonController extends Controller {
 
  public function __construct() {
   $this->middleware( 'auth' );
   $this->middleware( 'verified' );
- }
-
- public function express( Request $request, Adv $subject ) {
-  $this->validate( $request, [
-   'body' => [ 'required', 'string' ]
-  ] );
-
-  $opinion = $request->user()->express( $request->all(), $subject );
-
-  return back();
  }
 
  public function comment( Request $request, Opinion $opinion ) {
