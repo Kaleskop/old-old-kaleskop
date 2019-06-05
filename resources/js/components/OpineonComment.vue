e9c7c9d04a23a6909ccd6f955398167886b0afa3
Mindexperiment <andrewloyd@libero.it>
2019-06-05 16:26:32 +0200

    add opineon comment component

---
+++ resources/js/components/OpineonComment.vue
@@ -0,0 +1,16 @@
+<template>
+ 
+</template>
+
+<script>
+export default {
+ "name": "opineon-comment",
+
+  "props": {
+  "comment": {
+   "type": Object,
+   "required": true
+  }
+ }
+}
+</script>
