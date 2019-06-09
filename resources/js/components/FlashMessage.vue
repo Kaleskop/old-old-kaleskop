fdd5891e4d6a601ee5a384a44ce6f11122437877
Mindexperiment <andrewloyd@libero.it>
2019-06-09 11:40:10 +0200

    add flash message component

---
+++ resources/js/components/FlashMessage.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+ "name": "flash-message",
+
+ "props": {
+  "type": Object,
+  "required": true
+ },
+
+ data() {
+  return {
+   "body": ''
+  }
+ }
+}
+</script>
