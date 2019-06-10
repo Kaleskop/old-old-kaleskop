133b27cd8af0fa37f3c60405938c018a1cb6e981
Mindexperiment <andrewloyd@libero.it>
2019-06-10 16:25:18 +0200

    style opineon comment form component

--- resources/js/components/CommentForm.vue
+++ resources/js/components/CommentForm.vue
@@ -2,17 +2,17 @@
 
 <form v-on:submit.prevent="onSubmit" v-on:keydown="form.errors.clear()">
  <fieldset>
-  <legend>Comment details</legend>
+  <legend class="p-2 font-light italic hidden">Comment details</legend>
 
   <div>
-   <label for="body">Body</label>
-   <textarea name="body" id="body" placeholder="Your comment.." v-model="form.body" required></textarea>
+   <label for="body" class="block p-2 font-semibold hidden">Body</label>
+   <textarea name="body" id="body" class="appearance-none outline-none cursor-text block resize-y w-full leading-snug py-1 px-2 border border-transparent bg-white shadow focus:shadow-md" placeholder="Your comment.." v-model="form.body" required></textarea>
 
-   <p v-if="form.errors.has('body')" v-text="form.errors.get('body')"></p>
+   <p class="text-sm text-red-400" v-if="form.errors.has('body')" v-text="form.errors.get('body')"></p>
   </div>
  </fieldset>
 
- <button type="submit" v-bind:disabled="form.errors.any()">Comment</button>
+ <button type="submit" class="appearance-none outline-none cursor-pointer select-none block leading-snug p-2 ml-auto border border-transparent font-medium text-center focus:shadow-md" v-bind:disabled="form.errors.any()">Comment</button>
 </form>
 
 </template>
