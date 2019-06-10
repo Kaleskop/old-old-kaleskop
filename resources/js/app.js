a08989392917cdf601329f2ff6d14e74fa33010c
Mindexperiment <andrewloyd@libero.it>
2019-06-10 10:13:08 +0200

    add global vue events manager

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+// - global event management
+window.events = new Vue();
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
