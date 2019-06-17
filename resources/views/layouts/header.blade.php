3b021ee40f5f441dc5fb99c1097baec73c6af66f
Mindexperiment <andrewloyd@libero.it>
2019-06-17 15:16:18 +0200

    fix #91 - style menu items on header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,13 +2,13 @@
  <h1 class="font-semibold text-2xl"><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></h1>
 
  @guest
- <a href="{{ route( 'login' ) }}" class="{{ request()->routeIs( 'login' ) ? 'active ' : '' }}ml-auto mr-4 sm:hidden">
+ <a href="{{ route( 'login' ) }}" class="{{ request()->routeIs( 'login' ) ? 'active ' : '' }}w-8 h-8 inline-flex items-center justify-center border border-transparent rounded-full ml-auto mr-4 sm:hidden">
   <span class="far fa-user"></span>
   <span class="hidden">{{ __( 'Login' ) }}</span>
  </a>
  @endguest
 
- <a href="{{ route( 'website.channels' ) }}" class="{{ request()->routeIs( [ 'website.channels', 'website.advs' ] ) ? 'active ' : '' }}sm:hidden">
+ <a href="{{ route( 'website.channels' ) }}" class="{{ request()->routeIs( [ 'website.channels', 'website.advs' ] ) ? 'active ' : '' }}w-8 h-8 inline-flex items-center justify-center border border-transparent rounded-full sm:hidden">
   <span class="fas fa-project-diagram"></span>
   <span class="hidden">{{ __( 'Channels' ) }}</span>
  </a>
