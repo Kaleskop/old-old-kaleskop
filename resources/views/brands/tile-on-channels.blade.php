abf5d3f5f8ebafe128e92969046ae44dfb31da8a
Mindexperiment <andrewloyd@libero.it>
2019-06-17 20:13:05 +0200

    update brand tile on channel

--- resources/views/brands/tile-on-channels.blade.php
+++ resources/views/brands/tile-on-channels.blade.php
@@ -1,11 +1,7 @@
 <div class="relative w-32 sm:w-48 md:w-56">
  <figure>
   <img src="{{ asset( 'storage/'.$brand->logo_path ) }}" />
+
+  <figcaption>{{ $brand->name }}</figcaption>
  </figure>
-
- <article>
-  <h4>{{ $brand->name }}</h4>
-
-  <p>{{ Illuminate\Mail\Markdown::parse( $brand->description ) }}</p>
- </article>
 </div>
