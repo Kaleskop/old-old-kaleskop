59b493b90a72da6b2437f62243f9a68060e08e57
Mindexperiment <andrewloyd@libero.it>
2019-06-25 18:06:09 +0200

    link advs to advs page

--- resources/views/brands/tile-on-adv.blade.php
+++ resources/views/brands/tile-on-adv.blade.php
@@ -1,7 +1,9 @@
 <div class="p-2 {{ $adv->widthClassName() }}">
+<a href="{{ route( 'website.advs', [ 'brand'=>$brand, 'adv'=>$adv ] ) }}">
  <figure class="tile relative">
   <img src="{{ asset( 'storage/'.$brand->logo_path ) }}" />
 
   <figcaption class="absolute bottom-0 left-0 p-2 text-sm uppercase bg-black text-white">{{ $brand->name }}</figcaption>
  </figure>
+</a>
 </div>
