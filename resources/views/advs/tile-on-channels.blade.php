fb39edf66999dd12d24493402f2c964902f261e6
Mindexperiment <andrewloyd@libero.it>
2019-06-25 17:46:44 +0200

    style tile-on-channels

--- resources/views/advs/tile-on-channels.blade.php
+++ resources/views/advs/tile-on-channels.blade.php
@@ -1,4 +1,4 @@
-<article class="p-2 w-full md:w-1/2">
+<article class="p-2 w-full md:w-1/2 hover:shadow-lg hover:bg-grey-100">
  <div class="relative p-2 flex flex-wrap justify-center items-center">
   @forelse( $adv->brands as $brand )
    @include( 'brands.tile-on-adv' )
@@ -9,5 +9,5 @@
   @endforelse
  </div>
 
- <h4 class="mb-2 font-medium text-2xl">{{ $adv->title }}</h4>
+ <h4 class="mb-2 font-medium text-xl text-center">{{ $adv->title }}</h4>
 </article>
