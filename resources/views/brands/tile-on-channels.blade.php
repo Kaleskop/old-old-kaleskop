0d11337897a641dc4d73987c59d49a65ac41f1e8
Mindexperiment <andrewloyd@libero.it>
2019-06-25 16:54:29 +0200

    style figure and figcaption on brands tile-on-channels

--- resources/views/brands/tile-on-channels.blade.php
+++ resources/views/brands/tile-on-channels.blade.php
@@ -1,7 +1,7 @@
 <div class="relative w-32 sm:w-48 md:w-56">
- <figure>
+ <figure class="tile relative">
   <img src="{{ asset( 'storage/'.$brand->logo_path ) }}" />
 
-  <figcaption>{{ $brand->name }}</figcaption>
+  <figcaption class="absolute inset-x-0 bottom-0 p-2 text-sm uppercase bg-black text-white">{{ $brand->name }}</figcaption>
  </figure>
 </div>
