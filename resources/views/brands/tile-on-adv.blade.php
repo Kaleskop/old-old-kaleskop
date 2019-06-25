c18ca65abb9743dfeff06f2f8c051bf7b480f8ce
Mindexperiment <andrewloyd@libero.it>
2019-06-25 17:49:10 +0200

    style brands tile-on-adv

--- resources/views/brands/tile-on-adv.blade.php
+++ resources/views/brands/tile-on-adv.blade.php
@@ -1,7 +1,7 @@
 <div class="p-2 {{ $adv->widthClassName() }}">
- <figure>
+ <figure class="tile relative">
   <img src="{{ asset( 'storage/'.$brand->logo_path ) }}" />
 
-  <figcaption>{{ $brand->name }}</figcaption>
+  <figcaption class="absolute bottom-0 left-0 p-2 text-sm uppercase bg-black text-white">{{ $brand->name }}</figcaption>
  </figure>
 </div>
